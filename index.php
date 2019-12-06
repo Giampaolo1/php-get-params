c3506b61304fe32ddac545372ed93141be2efd4d
Giampaolo1 <giampaolo.r@outlook.com>
2019-12-06 14:43:10 +0100

    Esercizio

--- index.php
+++ index.php
@@ -2,3 +2,32 @@
 variabile password passata in GET è
 uguale a "Boolean", altrimenti stampare
 una stringa rossa. -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JP php pwd</title>
+
+    <?php
+
+      $pwd = $_GET ["pwd"];
+      // echo $pwd;
+
+     ?>
+
+  </head>
+  <body>
+
+    <?php
+
+      if ($pwd == "Boolean") {
+        echo "<h1 style='color:red'>"."pwd Corretta"."</h1>";
+      } else {
+        echo "<h1 style='color:blue'>"."pwd Incorretta"."</h1>";
+      }
+
+     ?>
+
+  </body>
+</html>
